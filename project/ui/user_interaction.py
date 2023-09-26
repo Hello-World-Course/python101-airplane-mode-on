ca15fa7819b757a24228e6e38c179c5f01f909d1
AviRon <avironavi87@gmail.com>
2023-09-27 01:32:40 +0300

    solved lesson 5 (1.3)

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,13 +1,12 @@
 name = input ("Hello, whats your name?")
 
-board_size = int (input (name + ", please choose board size"))
+board_size = int (input (name + ", please choose board size:"))
 
 board_size_str = str(board_size)
 
-number_of_mines = int (input (name + ", for board size " + board_size_str + ", choose number of mines to allocate"))
+number_of_mines = int (input (name + ", for board size " + board_size_str + ", choose number of mines to allocate:"))
 
 number_of_mines_str = str(number_of_mines)
 
 print(name + ", the board size is: " + board_size_str + ", number of mines is: " + number_of_mines_str + ", ENJOY!")
 
-#Michael, the board size is: 9, number of mines is: 7. ENJOY!
