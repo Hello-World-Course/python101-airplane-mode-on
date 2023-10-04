0a5f0a1d7200a21c89d69d7e5d2a91386d86cd2a
AviRon <avironavi87@gmail.com>
2023-10-04 12:43:38 +0300

    solved lesson 10 (1.5)

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -17,26 +17,22 @@ def is_number_of_mines_valid(board_size, number_of_mines):
 
 
 
+player_name = input("Hello, what's your name?")
 
-def register_user():
-    player_name = input("Hello, what's your name?")
-    if not is_name_valid(player_name):
-        print("Your name is too short")
-        player_name = None
+if not is_name_valid(player_name):
+    print("Your name is too short")
+    player_name = None
 
 
-    board_size = input(player_name + ", please choose board size:")
-    if not is_board_size_valid(int(board_size)):
-        print(player_name, "you entered illegal board size")
-        board_size = None
+board_size = input(player_name + ", please choose board size:")
+if not is_board_size_valid(int(board_size)):
+   print(player_name, "you entered illegal board size")
+   board_size = None
 
-    number_of_mines = input(player_name + ", for board size " + board_size + ", choose number of mines to allocate:")
-    if not is_number_of_mines_valid(int(board_size), int(number_of_mines)):
-        print(player_name, "you entered illegal number of mines")
-        number_of_mines = None
+number_of_mines = input(player_name + ", for board size " + board_size + ", choose number of mines to allocate:")
+if not is_number_of_mines_valid(int(board_size), int(number_of_mines)):
+   print(player_name, "you entered illegal number of mines")
+   number_of_mines = None
 
-    return player_name, int(board_size), int(number_of_mines)
+print(player_name + ", the board size is: " + board_size + ", number of mines is: " + number_of_mines + ", ENJOY!")
 
-player_name, board_size, mines_num = register_user()
-if player_name:
-    print(f"Registered Player: {player_name}, Board Size: {board_size}, Number of Mines: {mines_num}")
