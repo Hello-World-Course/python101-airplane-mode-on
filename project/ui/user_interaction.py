aba7dc93662be979ca01be78616c40e8c356e36e
AviRon <avironavi87@gmail.com>
2023-10-04 17:04:21 +0300

    solved lesson 10 (1.9)

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,14 +1,20 @@
 def is_name_valid(player_name):
     if len(player_name) > 2:
         return True
+    else:
+        return False
 
 def is_board_size_valid(board_size):
     if 0 < board_size <= 26:
         return True
+    else:
+        return False
 
 def is_number_of_mines_valid(board_size, number_of_mines):
     if 0 < number_of_mines <= (board_size / 2):
         return True
+    else:
+        return False
 
 def register_user():
     player_name = input("Hello, what's your name?")
@@ -20,14 +26,14 @@ def register_user():
 
     board_size = int(input(player_name + ", please choose board size:"))
     if not is_board_size_valid(board_size):
-        print(player_name, "you entered illegal board size")
+        print(player_name, ". you entered illegal board size")
         board_size = None
         return None, None, None
 
 
     number_of_mines = int(input(player_name + ", for board size " + str(board_size) + ", choose number of mines to allocate:"))
     if not is_number_of_mines_valid((board_size), (number_of_mines)):
-        print(player_name, "you entered illegal number of mines")
+        print(player_name, ", you entered illegal number of mines")
         number_of_mines = None
         return None, None, None
 
