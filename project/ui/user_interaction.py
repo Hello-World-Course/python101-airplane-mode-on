0a9078f0cf68527c5873e28ece58c9f771490002
AviRon <avironavi87@gmail.com>
2024-07-31 18:05:43 +0300

    #6

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,32 +1,32 @@
-# THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
+name = str(input("Hello, what's your name?"))
 
-name = str(input("Hello, whats your name"))
-
-# name length check:
-
-(name_length) = len(name)
-
-if name_length < 3:
+# name length check
+if len(name) < 3:
     print("Your name is too short")
     name = None
-
 else:
-    board_size = int(input(f"{name}, please choose board size"))
+    try:
+        board_size = int(input(f"{name}, please choose board size (between 1 and 25): "))
 
-# bord size check:
+        # board size check
+        if board_size < 1 or board_size >= 26:
+            print(f"{name}, you have entered illegal board size")
+            board_size = None
+        else:
+            try:
+                number_of_mines = int(
+                    input(f"{name}, for board size {board_size}, choose number of mines to allocate: "))
 
-if (board_size < 1) or board_size >= 26:
-    print(f"{name}, you have entered illegal board size")
-    board_size = None
+                # number of mines check
+                if number_of_mines < 1 or number_of_mines > (board_size * board_size) / 2:
+                    print(f"{name}, you have entered illegal number of mines")
+                    number_of_mines = None
+                else:
+                    print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}, ENJOY!")
+            except ValueError:
+                print(f"{name}, you have entered illegal number of mines")
+                number_of_mines = None
 
-else:
-    number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
-
-# number of mines check:
-
-if (number_of_mines < 1) or  number_of_mines >= (board_size / 2):
-    print(f"{name}, you have entered illegal number of mines")
-    number_of_mines = None
-
-else:
-    print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}, ENJOY!")
+    except ValueError:
+        print(f"{name}, you have entered illegal board size")
+        board_size = None
