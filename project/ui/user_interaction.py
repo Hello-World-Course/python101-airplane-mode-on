71e785f5c2b260f417010b9d3ac7fa6bedae6a1f
AviRon <avironavi87@gmail.com>
2024-07-31 17:26:03 +0300

    #1

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,5 +1,32 @@
 # THIS CODE IS WRONG, FIX IT AND ADD NEW CODE
+
+
 name = input("Hello, whats your name")
-board_size = int(input(f"{name}, please choose board size"))
-number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
-print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}, ENJOY!")
+
+# name length check:
+
+name_length = len(name)
+if name_length <= 2:
+    print("Your name is too short")
+    name = None
+
+else:
+    board_size = int(input(f"{name}, please choose board size"))
+
+# bord size check:
+
+if ((board_size % 1) != 0) or board_size >= 26:
+    print(f"{name}, you have entered illegal board size")
+    board_size = None
+
+else:
+    number_of_mines = int(input(f"{name}, for board size {board_size}, choose number of mines to allocate"))
+
+# number of mines check:
+
+if (number_of_mines < 0) or  number_of_mines > (board_size / 2):
+    print(f"{name}, you have entered illegal number of mines")
+    number_of_mines = None
+
+else:
+    print(f"{name}, the board size is: {board_size}, number of mines is: {number_of_mines}, ENJOY!")
