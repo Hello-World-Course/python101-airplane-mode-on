cd75452b80a26a054b8c098dc8cc46715e13952e
AviRon <avironavi87@gmail.com>
2024-08-04 10:50:53 +0300

    #7

--- project/ui/user_interaction.py
+++ project/ui/user_interaction.py
@@ -1,30 +1,40 @@
+# name length check
 def is_name_valid(name):
-    return len(name) >= 3
+    if len(name) < 3:
+        return None
+    return name
 
 # board size check
 def is_board_size_valid(board_size):
-    return 1 <= board_size < 26
+    if board_size < 1 or board_size >= 26:
+        return None
+    return board_size
 
 # number of mines check
 def is_number_of_mines_valid(board_size, number_of_mines):
-    return  1 <= number_of_mines <= ((board_size * board_size) / 2)
+    if number_of_mines < 1 or number_of_mines > ((board_size * board_size) / 2):
+        return None
+    return number_of_mines
 
 def register_user (name, board_size, number_of_mines):
     name = str(input("Hello, what's your name?"))
-    if not is_name_valid(name):
+    name = is_name_valid(name)
+    if name is not None:
+
+        board_size = int(input(f" please choose board size, {name}"))
+        board_size = is_board_size_valid(board_size)
+
+        if board_size is not None:
+            number_of_mines = int(input(f"for board size {board_size}, choose number of mines to allocate, {name}"))
+            number_of_mines = is_number_of_mines_valid(board_size, number_of_mines)
+
+            if number_of_mines is None:
+                print(f"{name}, you have entered illegal number of mines")
+        else:
+            print(f"{name}, you have entered illegal board size")
+
+    else:
         print("Your name is too short")
-        return (None, None, None)
-
-
-    board_size = int(input(f"please choose board size, {name}"))
-    if not is_board_size_valid(board_size):
-        print(f"{name}, you have entered illegal board size")
-        return (None, None, None)
-
-    number_of_mines = int(input(f"for board size {board_size}, choose number of mines to allocate, {name}"))
-    if not is_number_of_mines_valid(board_size, number_of_mines):
-        print(f"{name}, you have entered illegal number of mines")
-        return (None, None, None)
 
     return name, board_size, number_of_mines
 
