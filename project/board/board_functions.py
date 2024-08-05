ee695f50ea2af96f2c6c59b766db9e67d3a49cf5
AviRon <avironavi87@gmail.com>
2024-08-05 23:39:38 +0300

    #7

--- project/board/board_functions.py
+++ project/board/board_functions.py
@@ -1,7 +1,7 @@
 
 
 def is_on_board(x, y, board):
-    if x < 0 or x >= len(board) or y < 0 or y >= len(board):
+    if x < 0 or x >= len(board) or y < 0 or y >= len(board[0]):
         return False
     return True
 
